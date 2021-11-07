2672c926b3210e138167f2712005ecf2f40967a2
“joemarin <joseph.rodriguez.marin@gmail.com>
2021-11-06 19:13:17 -0600

    Initial

---
+++ src/main/java/com/joe/spring5web/HomeController.java
@@ -0,0 +1,13 @@
+package com.joe.spring5web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String home(){
+        return "Hello Joe!";
+    }
+}
