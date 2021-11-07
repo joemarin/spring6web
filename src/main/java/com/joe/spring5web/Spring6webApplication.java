a44ebb58f46b2801ab247a0a4a6bedeabd1d8052
“joemarin <joseph.rodriguez.marin@gmail.com>
2021-11-07 17:41:45 -0600

    Test 1

---
+++ src/main/java/com/joe/spring5web/Spring6webApplication.java
@@ -0,0 +1,13 @@
+package com.joe.spring5web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6webApplication.class, args);
+	}
+
+}
