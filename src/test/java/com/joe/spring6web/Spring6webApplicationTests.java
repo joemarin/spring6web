bb002fba235379de0b63d659f733e50bd0e64753
“joemarin <joseph.rodriguez.marin@gmail.com>
2021-11-13 21:08:28 -0600

    Test 2

--- src/test/java/com/joe/spring6web/Spring6webApplicationTests.java
+++ src/test/java/com/joe/spring6web/Spring6webApplicationTests.java
@@ -1,10 +1,10 @@
-package com.joe.spring5web;
+package com.joe.spring6web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring5webApplicationTests {
+class Spring6webApplicationTests {
 
 	@Test
 	void contextLoads() {
